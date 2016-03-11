a51ed0f63355fe3e7e57f1f42053995d2c2e5539
John H <john.hogenmiller@emc.com>
2016-03-11 00:13:23 -0500

    more line unwrapping

--- extra/make-testcoveralls.sh
+++ extra/make-testcoveralls.sh
@@ -8,6 +8,5 @@ then
    exit 1
 fi
 
-./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha --report \ 
-  lcovonly -- $(find spec -name '*-spec.js') -R spec --require spec/helper.js
+./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha --report lcovonly -- $(find spec -name '*-spec.js') -R spec --require spec/helper.js
 cat ./coverage/lcov.info | node_modules/.bin/coveralls
