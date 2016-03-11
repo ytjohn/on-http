6ebcfe105324f5cfa06f392b30a690d8c61b0a36
John H <john.hogenmiller@emc.com>
2016-03-10 23:13:40 -0500

    fixing line wraps

--- extra/make-deb.sh
+++ extra/make-deb.sh
@@ -12,12 +12,12 @@ BRANCH=$(git symbolic-ref --short -q HEAD)
 
 npm prune --production
 pushd ./swagger-codegen && mvn package && popd
-java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i static/monorail.yml -o on-http-api1.1
- -l python --additional-properties packageName=on_http_api1_1
-java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i static/monorail-2.0.yaml -o on-http-a
-pi2.0 -l python --additional-properties packageName=on_http_api2_0
-java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i static/redfish.yaml -o on-http-redfis
-h-1.0 -l python --additional-properties packageName=on_http_redfish_1_0
+java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i static/monorail.yml \ 
+  -o on-http-api1.1 -l python --additional-properties packageName=on_http_api1_1
+java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i static/monorail-2.0.yaml \ 
+  -o on-http-api2.0 -l python --additional-properties packageName=on_http_api2_0
+java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i static/redfish.yaml \ 
+  -o on-http-redfish-1.0 -l python --additional-properties packageName=on_http_redfish_1_0
 ./build-package.bash python-client "${BRANCH}" "on-http-api1.1"
 ./build-package.bash python-client "${BRANCH}" "on-http-api2.0"
 ./build-package.bash python-client "${BRANCH}" "on-http-redfish-1.0"
--- extra/make-testcoveralls.sh
+++ extra/make-testcoveralls.sh
@@ -8,6 +8,6 @@ then
    exit 1
 fi
 
-./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha --report lcovonly -- $(find spec -name '*-spec.js') -R spe
-c --require spec/helper.js
+./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha --report \ 
+  lcovonly -- $(find spec -name '*-spec.js') -R spec --require spec/helper.js
 cat ./coverage/lcov.info | node_modules/.bin/coveralls
